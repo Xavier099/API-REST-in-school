6c2332f19a6d82c526204abe128c17701886db1e
soldado-pratiado <nycollas.queiroz@escola.pr.gov.br>
2026-05-07 07:32:03 -0300

    first commit

---
+++ src/main/java/com/xavier/BurguerKing/BurguerKingApplication.java
@@ -0,0 +1,13 @@
+package com.xavier.BurguerKing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurguerKingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurguerKingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xavier/BurguerKing/BurguerKingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xavier.BurguerKing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurguerKingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
